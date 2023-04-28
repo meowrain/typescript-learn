be3a94b85c69b5e5f877cd43275a0ce2d4fa3d1c
meowrain <meowrain@126.com>
2023-04-28 19:15:25 +0800

    LESSON2 update

---
+++ LESSON02/build/js/main.js
@@ -0,0 +1,14 @@
+"use strict";
+let myName = 'MeowRain';
+let age = 18;
+let isLoading = true;
+let album;
+let postId;
+let isActive;
+album = 1;
+album = 'fdsafsd';
+album = true;
+const sum = (a, b) => {
+    return a + b;
+};
+let re = /\w+/g;
---
+++ LESSON02/src/main.ts
@@ -0,0 +1,13 @@
+let myName: string = 'MeowRain';
+let age: number = 18;
+let isLoading: boolean = true;
+let album: any;
+let postId: number | string;
+let isActive: number | boolean;
+album = 1;
+album = 'fdsafsd';
+album = true;
+const sum = (a: number, b: number): number => {
+    return a + b;
+}
+let re: RegExp = /\w+/g
